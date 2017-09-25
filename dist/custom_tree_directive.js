2a58291eda3fd05c9fed15693c920f3a3ce806ab
xiaokeke <2399298816@qq.com>
2017-09-25 15:05:43 +0800

    test

--- dist/custom_tree_directive.js
+++ dist/custom_tree_directive.js
@@ -2,7 +2,7 @@
   var module,
     __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
-  module = angular.module('angularBootstrapNavTree', []);
+  module = angular.module('angularBootstrapNavTreeCustom', []);
 
   module.directive('customTree', [
     '$timeout', function($timeout) {
